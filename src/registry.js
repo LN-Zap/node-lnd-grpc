bfa925927cab0fb1cd79d7be9ff326b2bd612cb2
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-07-26 16:09:39 +0100

    fix: add v0.7.1 to services registry

--- src/registry.js
+++ src/registry.js
@@ -9,68 +9,75 @@ const commonServices = [
   },
 ]
 
+const v7Services = [
+  ...commonServices,
+  {
+    name: 'Autopilot',
+    proto: 'autopilotrpc/autopilot.proto',
+  },
+  {
+    name: 'ChainNotifier',
+    proto: 'chainrpc/chainnotifier.proto',
+  },
+  {
+    name: 'Invoices',
+    proto: 'invoicesrpc/invoices.proto',
+  },
+  {
+    name: 'Router',
+    proto: 'routerrpc/router.proto',
+  },
+  {
+    name: 'Signer',
+    proto: 'signrpc/signer.proto',
+  },
+  {
+    name: 'WalletKit',
+    proto: 'walletrpc/walletkit.proto',
+  },
+  {
+    name: 'Watchtower',
+    proto: 'watchtowerrpc/watchtowerrpc.proto',
+  },
+]
+
+const v6Servicves = [
+  ...commonServices,
+  {
+    name: 'Autopilot',
+    proto: 'autopilotrpc/autopilot.proto',
+  },
+  {
+    name: 'ChainNotifier',
+    proto: 'chainrpc/chainnotifier.proto',
+  },
+  {
+    name: 'Invoices',
+    proto: 'invoicesrpc/invoices.proto',
+  },
+  {
+    name: 'Router',
+    proto: 'routerrpc/router.proto',
+  },
+  {
+    name: 'Signer',
+    proto: 'signrpc/signer.proto',
+  },
+  {
+    name: 'WalletKit',
+    proto: 'walletrpc/walletkit.proto',
+  },
+]
+
 export default {
+  '0.7.1-beta': {
+    services: v7Services,
+  },
   '0.7.0-beta': {
-    services: [
-      ...commonServices,
-      {
-        name: 'Autopilot',
-        proto: 'autopilotrpc/autopilot.proto',
-      },
-      {
-        name: 'ChainNotifier',
-        proto: 'chainrpc/chainnotifier.proto',
-      },
-      {
-        name: 'Invoices',
-        proto: 'invoicesrpc/invoices.proto',
-      },
-      {
-        name: 'Router',
-        proto: 'routerrpc/router.proto',
-      },
-      {
-        name: 'Signer',
-        proto: 'signrpc/signer.proto',
-      },
-      {
-        name: 'WalletKit',
-        proto: 'walletrpc/walletkit.proto',
-      },
-      {
-        name: 'Watchtower',
-        proto: 'watchtowerrpc/watchtowerrpc.proto',
-      },
-    ],
+    services: v7Services,
   },
   '0.6.0-beta': {
-    services: [
-      ...commonServices,
-      {
-        name: 'Autopilot',
-        proto: 'autopilotrpc/autopilot.proto',
-      },
-      {
-        name: 'ChainNotifier',
-        proto: 'chainrpc/chainnotifier.proto',
-      },
-      {
-        name: 'Invoices',
-        proto: 'invoicesrpc/invoices.proto',
-      },
-      {
-        name: 'Router',
-        proto: 'routerrpc/router.proto',
-      },
-      {
-        name: 'Signer',
-        proto: 'signrpc/signer.proto',
-      },
-      {
-        name: 'WalletKit',
-        proto: 'walletrpc/walletkit.proto',
-      },
-    ],
+    services: v6Servicves,
   },
   '0.5.2-beta': {
     services: commonServices,
