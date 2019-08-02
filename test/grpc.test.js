9c4e238e5625e95f776e6c438b4a0e81961babd7
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-08-02 13:41:18 +0100

    refactor: clean up tests

--- test/grpc.test.js
+++ test/grpc.test.js
@@ -2,7 +2,6 @@ import { spawn } from 'child_process'
 import test from 'tape-promise/tape'
 import sinon from 'sinon'
 import { join } from 'path'
-import { status, ClientReadableStream } from '@grpc/grpc-js'
 import LndGrpc from '../src'
 
 const hostname = 'testnet3'
@@ -112,33 +111,4 @@ test('ready -> disconnect', async t => {
     t.equal(e.message, 'transition is invalid in current state', 'should throw an error if called from ready state')
     t.ok(e.stack, 'error has stack')
   }
-})
-
-test('Lightning.invoices', async t => {
-  t.plan(3)
-  let grpc, call
-  try {
-    grpc = new LndGrpc(grpcOptions)
-    await grpc.connect()
-    call = grpc.services.Lightning.subscribeInvoices()
-    t.equal(
-      call.constructor.name,
-      'ClientReadableStreamImpl',
-      'Lightning.subscribeInvoices() should return a ClientReadableStreamImpl instance',
-    )
-    const promise = new Promise(async (resolve, reject) => {
-      call.on('error', function(error) {
-        t.equal(error.code, status.CANCELLED, 'call.cancel() should cancel an invoice subscription stream')
-        if (error.code === status.CANCELLED) {
-          return resolve()
-        }
-      })
-    })
-    await grpc.services.Lightning.addInvoice({ value: 100 })
-    await promise
-  } catch (e) {
-    t.equal(e.message, 'permission denied', 'should not allow creating an invoice with a readonly macaroon')
-  }
-  call && call.cancel()
-  await grpc.disconnect()
 })
