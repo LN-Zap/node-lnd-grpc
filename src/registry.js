6c0a3fb0f0a80943e89900271d231e548ef9d274
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-10-01 10:06:00 +0200

    fix: add 0.8.0-alpha-zap to registry

--- src/registry.js
+++ src/registry.js
@@ -9,38 +9,6 @@ const commonServices = [
   },
 ]
 
-const v7Services = [
-  ...commonServices,
-  {
-    name: 'Autopilot',
-    proto: 'autopilotrpc/autopilot.proto',
-  },
-  {
-    name: 'ChainNotifier',
-    proto: 'chainrpc/chainnotifier.proto',
-  },
-  {
-    name: 'Invoices',
-    proto: 'invoicesrpc/invoices.proto',
-  },
-  {
-    name: 'Router',
-    proto: 'routerrpc/router.proto',
-  },
-  {
-    name: 'Signer',
-    proto: 'signrpc/signer.proto',
-  },
-  {
-    name: 'WalletKit',
-    proto: 'walletrpc/walletkit.proto',
-  },
-  {
-    name: 'Watchtower',
-    proto: 'watchtowerrpc/watchtowerrpc.proto',
-  },
-]
-
 const v6Servicves = [
   ...commonServices,
   {
@@ -69,7 +37,26 @@ const v6Servicves = [
   },
 ]
 
+const v7Services = [
+  ...v6Servicves,
+  {
+    name: 'Watchtower',
+    proto: 'watchtowerrpc/watchtowerrpc.proto',
+  },
+]
+
+const v8Services = [
+  ...v7Services,
+  {
+    name: 'WatchtowerClient',
+    proto: 'wtclientrpc/wtclientrpc.proto',
+  },
+]
+
 export default {
+  '0.8.0-alpha-zap': {
+    services: v7Services,
+  },
   '0.7.1-beta': {
     services: v7Services,
   },
