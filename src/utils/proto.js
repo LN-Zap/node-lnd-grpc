597689a65dc03255c37a6f819fd9147e60b22434
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2021-12-12 10:53:03 +0100

    Add support for lnd 0.14.1-beta

--- src/utils/proto.js
+++ src/utils/proto.js
@@ -6,7 +6,7 @@ import registry from '../registry'
 const debug = createDebug('lnrpc:proto')
 
 export const GRPC_LOWEST_VERSION = '0.4.2-beta'
-export const GRPC_HIGHEST_STABLE_VERSION = '0.14.0-beta'
+export const GRPC_HIGHEST_STABLE_VERSION = '0.14.1-beta'
 
 /**
  * Get the directory where rpc.proto files are stored.
